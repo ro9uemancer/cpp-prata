5d27028959a328202aee4ca20668c71b8243593f
ro9uemancer <ro9uemancer@gmail.com>
2016-03-15 20:52:56 +0100

    01 exercise

---
+++ c02_e01_project001/Source.cpp
@@ -0,0 +1,19 @@
+// name: c02_e01_project001
+// tags: #cout
+// author: ro9uemancer
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "John Doe" << endl;
+	cout << "354 Wayback Lane" << endl;
+	cout << "New York, NY 10001" << endl;
+
+	_getch();
+	return 0;
+}
